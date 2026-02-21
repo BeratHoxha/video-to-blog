147bd900910f74b99839d5341bf40a7e361fe64c
Berat Hoxha <berathoxha4@gmail.com>
2026-02-21 18:46:35 +0100

    General fixes in configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
@@ -34,7 +36,5 @@ export default {
       },
     },
   },
-  plugins: [
-    (await import("tailwindcss-animate")).default,
-  ],
+  plugins: [tailwindcssAnimate],
 };
